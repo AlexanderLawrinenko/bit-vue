c8b8c0d0458caef143094f824ba6b9cb3851e2b0
Alexandr Lawrinenko <lawrinenko.frontend@gmail.com>
2021-02-09 23:20:56 +0200

    [ADD] add Warrants

--- src/components/Warrants.vue
+++ src/components/Warrants.vue
@@ -1,7 +1,56 @@
 <template>
-  <div>warrants</div>
+  <div class="container-form" v-if="getQuote">
+    <div class="container-form__control">
+      <input class="container-form__control-item form-control" @keypress="checkCellNum" v-model="order" type="text">
+    </div>
+    <div class="container-form__actions">
+      <button @click="sendOrder('buy')" type="button" :disabled="validation" class="base-btn">Buy</button>
+      <button @click="sendOrder('sell')" type="button" :disabled="validation" class="base-btn">Sell</button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import { mapGetters } from 'vuex';
+
+export default {
+  data () {
+    return {
+      orderQty: 1
+    }
+  },
+  computed: {
+    ...mapGetters(['getQuote']),
+    validation () {
+      if (typeof this.orderQty === 'number' && this.orderQty > 0) return false
+      else return true
+    },
+    order: {
+      get: function() {
+        return this.orderQty
+      },
+      set: function(val) {
+        const num = Number(val)
+        if (typeof num === 'number' && !isNaN(num) ) {
+          this.orderQty = num
+        } else {
+          this.orderQty = 0
+        }
+      }
+    }
+  },
+  methods: {
+    sendOrder (marker) {
+      console.log(this.orderQty, marker);
+    },
+    checkCellNum(evt) {
+      let regex = new RegExp("^[0-9.]+$");
+      let key = String.fromCharCode(!evt.charCode ? evt.which : evt.charCode);
+      if (!regex.test(key)) {
+        event.preventDefault();
+        return false;
+      }
+    }
+  }
+}
 </script>
